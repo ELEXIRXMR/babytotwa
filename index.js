d3e5256a47500157c81e16ef06ce30260d20eeb9
ELEXIRXMR <74086780+ELEXIRXMR@users.noreply.github.com>
2021-02-12 17:10:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -41,23 +41,23 @@ const vcard = 'BEGIN:VCARD\n'
 // KASIH WM GUE
             + 'VERSION:3.0\n' 
 // RAMLAN ID
-            + 'FN:RAMLAN ID\n' 
+            + 'FN:VBOT_HENTAI\n' 
 // JAN NGEYEL AJG
             + 'ORG:OWNER BOTZ;\n'
-            + 'TEL;type=CELL;type=VOICE;waid=6285559240360:+62 855-5924-0360\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=6288219149952:+62 882-1914-9952\n' 
             + 'END:VCARD' 
 prefix = '!'
 blocked = []   
 limitawal = 20
 memberlimit = 2
-cr = '*𝐁𝐀𝐁𝐘 𝐁𝐎𝐓𝐙 𝐕𝐄𝐑𝐈𝐅𝐈𝐄𝐃*'
+cr = '*VBOT_HENTAI 𝐕𝐄𝐑𝐈𝐅𝐈𝐄𝐃*'
 
 /*
 ]=====> SETTINGS <=====[
 */
-const ownerNumber = ["6285559240360@s.whatsapp.net","6285559240360@s.whatsapp.net"]
-const botName = '𝐁𝐀𝐁𝐘 𝐁𝐎𝐓𝐙'
-const ownerName = '𝗥𝗮𝗺𝗹𝗮𝗻 𝗜𝗗'
+const ownerNumber = ["6288219149952@s.whatsapp.net","6288219149952@s.whatsapp.net"]
+const botName = 'VBOT_HENTAI'
+const ownerName = 'MAHA'
 const BarBarKey = 'IDxO1TFYnKADlX4pxcHa' // APIKEY DOWNLOAD FILE
 const VhtearKey = 'APIKEY' // YANG DI YOUTUBE
 const TobzKey = 'BotWeA' // JANGAN MAU ENAK DOANG
